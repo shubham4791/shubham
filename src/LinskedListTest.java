7ae5a36a2d76b86dfc837cb523889220f534cdf8
shubham4791 <139962000+shubham4791@users.noreply.github.com>
2023-07-19 14:35:37 +0530

    1st code

---
+++ src/LinskedListTest.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinskedListTest {
+
+	public static void main(String[] args) {
+		
+	
+		
+		List<Integer> list=new LinkedList ();
+		list.add(10);
+		list.add(12);
+		list.add(13);
+		
+		for(Integer o:list)
+		{
+			System.out.println(o+1);
+		}
+		
+	}
+}
